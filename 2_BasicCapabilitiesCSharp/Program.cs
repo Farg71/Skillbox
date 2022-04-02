a07445f73cf27dec3ddf82230d7f5d2f15790f63
Sergejs Ivanovs <sergej_iv@inbox.lv>
2022-04-02 14:16:23 +0300

    initial

---
+++ 2_BasicCapabilitiesCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿
+#region Using
+using System;
+#endregion
+
+namespace BasicCapabilitiesCSharp
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Иванов Иван Иванович";           // ФИО
+            string eMail = "ivan@ivanov";                       // ФИО, эл.почта
+            int age = 125;                                      // возраст
+            float gradeProgramming = 75.3f;                     // оценка  по программированию
+            float gradeMath = 99.95f;                           // оценка по математике
+            float gradePhisics = 34.11f;                        // оценка по физике
+
+            Console.Clear();
+            Console.Write("Введите номер домашнего задания ('1' или '2'; другие - завершение проверки): ");
+
+            Console.WriteLine($"Студент: {fullName}, возраст: {age}, эллектронный адрес: {eMail}.");
+            Console.ReadKey();
+
+            var summGrades = gradeMath + gradePhisics + gradeProgramming;
+            var averageGrades = (double) summGrades / 3d;
+
+            var pattern = "Студент {0} набрал суииу баллов - {1} и получил средний балл {2}";
+
+
+            Console.WriteLine(pattern,
+                                fullName,
+                                summGrades,
+                                averageGrades);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,66 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Импорт сторонних пространст имён
+using System;
+
+/// <summary>
+/// Пространство имён
+/// Создание собственной библиотеки
+/// </summary>
+namespace MyHelloWorldConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа
+        /// Статический метод, не возвращает значение
+        /// </summary>
+        /// <param name="args">строковый массив параметров</param>
+        static void Main(string[] args)
+        {
+            string? taskNumber = "1";
+
+            while (taskNumber == "1" || taskNumber == "2")
+            {
+                Console.Write("Введите номер домашнего задания ('1' или '2'; другие - завершение проверки): ");
+
+                //ConsoleKeyInfo taskNumber = Console.ReadKey();
+                //Console.WriteLine(taskNumber.ToString());
+
+                taskNumber = Console.ReadLine();
+                //string taskNumber = "1";
+
+                switch (taskNumber)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Hello World!!!");
+
+                            ConsoleKeyInfo keyInfo = Console.ReadKey();
+                        }
+                        break;
+
+                    case "2":
+                        {
+                            Console.Write("Hello ");
+                            Console.Write("World");
+                            Console.Write("!!!");
+
+                            string? readLine = Console.ReadLine();
+                        }
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Нет такого задания!! :)");
+                        Console.ReadLine();
+
+                        break;
+                }
+            }
+
+        }
+    }
+}
