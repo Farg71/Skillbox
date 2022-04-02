e2fc82216231f10a9fb216a3c827eff8b15cd756
Sergejs Ivanovs <sergej_iv@inbox.lv>
2022-04-02 14:26:38 +0300

    Проверка GIT

--- 2_BasicCapabilitiesCSharp/Program.cs
+++ 2_BasicCapabilitiesCSharp/Program.cs
@@ -34,6 +34,9 @@ namespace BasicCapabilitiesCSharp
                                 averageGrades);
             Console.ReadLine();
 
+
+            // Проверка GIT
+
             
         }
     }
