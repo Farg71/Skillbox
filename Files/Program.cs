a34c26359b2cbc215a9e29d07376a56d03b20700
Sergejs Ivanovs <sergej_iv@inbox.lv>
2022-04-03 21:22:50 +0300

    files

---
+++ Files/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Files
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            string? taskNumber = "1";
+
+            string filePath = @"..\..\..\Staff.txt";
+            bool isCatalogExists = File.Exists(filePath);
+            int lastRecord = 1;
+
+
+            Console.WriteLine("\t\tСправочник «Сотрудники»\n");
+
+            while (taskNumber == "1" || taskNumber == "2")
+            {
+
+                Console.Clear();
+                Console.Write("Выберите режим работы (1 -вывести данные на экран, 2 - заполнить данные и добавить в справочник, " +
+                    "другие - завершение работы):\n");
+
+                taskNumber = Console.ReadLine();
+                Console.WriteLine("\n\n");
+
+                switch (taskNumber)
+                {
+                    case "1":
+                        {
+                            if (!isCatalogExists)
+                            {
+                                Console.WriteLine("Справочник отсутствует!");
+                            }
+                            else
+                            {
+                                string[] records = File.ReadAllLines(filePath);
+
+                                foreach (string employee in records)
+                                {
+                                    string[] record = employee.Split('#');
+                                    foreach(string cell in record )
+                                    {
+                                        Console.Write(cell + "\t");
+                                        //Console.Write($"{cell,30}");
+                                    }
+
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                        break;
+
+                    case "2":
+                        {
+                            if (!isCatalogExists)
+                            {
+                                lastRecord = 1;
+                            }
+                            else
+                            {
+                                string[] records = File.ReadAllLines(filePath);
+
+                                lastRecord = int.Parse(records[records.Length - 1].Split('#')[0]);
+                            }
+
+                            AddNewEmployees(filePath, lastRecord);
+
+                        }
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Удачного дня!");
+
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void AddNewEmployees(string filePath, int lastRecord)
+        {
+            Console.WriteLine("Сколько хотите добавить сотрудников?");
+
+            int employeesNumber = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < employeesNumber; i++)
+            {
+                string entryTime = DateTime.Now.ToString("g") + "#";
+
+                Console.WriteLine("\nВведите Ф.И.О. сотрудника");
+                string fullName = Console.ReadLine() + "#";
+
+                Console.WriteLine("\nВведите возраст сотрудника");
+                string age = Console.ReadLine() + "#";
+
+                Console.WriteLine("\nВведите рост сотрудника");
+                string growth = Console.ReadLine() + "#";
+
+                Console.WriteLine("\nВведите дату рождения сотрудника");
+                string dateBirth = Console.ReadLine() + "#";
+
+                Console.WriteLine("\nВведите место рождения сотрудника");
+                string placeBirth = Console.ReadLine() + "\n";
+
+                int newRecord = lastRecord + 1 + i;
+
+                string txt = newRecord.ToString() + "#" + entryTime + fullName + age + growth + dateBirth + placeBirth;
+
+                File.AppendAllText(filePath, txt);
+            }
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            string? taskNumber = "1";
+
+            while (taskNumber == "1" || taskNumber == "2")
+            {
+                Console.Clear();
+                Console.Write("Введите номер домашнего задания ('1' или '2'; другие - завершение проверки): ");
+
+                taskNumber = Console.ReadLine();
+
+                switch (taskNumber)
+                {
+                    // Задание 1. Метод разделения строки на слова
+                    case "1":
+                        {
+                            string inputPhrase = SentencesReading();                // Ввод предложения
+
+                            string[] words = SplittingStringIntoWords(inputPhrase); // Разделение предложения на слова
+
+                            WritesWords(words);                                     // Печать каждого слова в новой строке
+
+                        }
+                        break;
+
+                    // Задание 2. Перестановка слов в предложении
+                    case "2":
+                        {
+                            string inputPhrase = SentencesReading();                // Ввод предложения
+
+                            string outputPhrase = ReversWords(inputPhrase);         // Получение предложения в обратной последовательности
+
+                            Console.WriteLine($"Предложение а обратной последовательности:\n{outputPhrase}");
+                        }
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Нет такого задания!! :)");
+
+                        break;
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+
+        private static string ReversWords(string inputPhrase)
+        {
+            string resultString = "";
+
+            string[] words = SplittingStringIntoWords(inputPhrase); // Разделение предложения на слова
+
+            for(int i = words.Length -1; i >=0; i--)
+            {
+                resultString += words[i] + " ";
+            }
+
+            return resultString;
+        }
+
+        private static string SentencesReading()
+        {
+            Console.WriteLine("\n\t\tВведите предложение");
+
+            string inputPhrase = Console.ReadLine();
+
+            return inputPhrase;
+        }
+
+        private static void WritesWords(string[] words)
+        {
+            Console.WriteLine("\n\t\tПолученные слова:\n\n");
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        private static string[] SplittingStringIntoWords(string? inputPhrase)
+        {
+            string[] words = inputPhrase.Split(' ');
+
+            return words;
+        }
+    }
+}
+
